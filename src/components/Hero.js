0822276d15631d5d24fb87bc815153cd13a34f1b
dmartind <dmartind@umd.edu>
2022-03-04 14:58:37 -0500

    Learning Api

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,18 @@
 import Button from 'react-bootstrap/Button';
-
+import $ from 'jquery';
+function heroList() {
+  $.getJSON('https://api.opendota.com/api/heroes')
+    .then(({ results }) => {
+    var json = {results};
+    console.log(results[0].id);});
+}
 function Hero() {
   return (
-    <div id = "hero">Hero<Button variant = "primary" onClick={() => console.log("deleteThis")}>X</Button></div>
+    <div id = "hero">Hero<Button style= {{width: "50px"}} variant = "danger" onClick={heroList}>X</Button></div>
 
   );
 }
+
+
 
 export default Hero;
